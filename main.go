1510f990196ab002fe088077af9ae814524ed0b0
MatteoTemp <matteotempella20@gmail.com>
2024-11-03 11:16:22 +0100

    .

--- main.go
+++ main.go
@@ -9,11 +9,11 @@ import (
 func main() {
 
 	equivalent := circuits.RealLCSeries{
-		R: dipoles.Resistor{Resistance: 100},
+		R: dipoles.Resistor{Resistance: 10},
 		C: dipoles.Capacitor{Capacitance: 1e-6},
 		L: dipoles.Inductor{Inductance: 1e-3},
 	}
 
 	circuits.Nyquist_plot(equivalent, "OutputFiles/Nyq.csv", 0, 6.0, 100)
-	circuits.Bode(equivalent, "OutputFiles/Bode.csv", 0, 6.0, 100)
+	circuits.Bode(equivalent, "OutputFiles/Bode.csv", 2, 6.0, 100)
 }
