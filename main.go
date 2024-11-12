5920aa4ad74f1c232386132198bb4cca8d308ac3
MatteoTemp <matteotempella20@gmail.com>
2024-11-12 14:59:32 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,20 +1,9 @@
 package main
 
-import (
-	circuits "goEl/curcuits"
-	dipoles "goEl/lib"
-	//cmath "math/cmplx"
-)
+//cmath "math/cmplx"
 
 func main() {
 
-	ew := circuits.RCLBandpass{
-		R: dipoles.Resistor{Resistance: 10},
-		C: dipoles.Capacitor{Capacitance: 1e-9},
-		L: dipoles.Inductor{Inductance: 1e-1},
-	}
 
-	circuits.Nyquist_plot(ew, "OutputFiles/Nyq.csv", -0, 6, 100)
-	circuits.Bode(ew, "OutputFiles/Bode.csv", -3, 12, 50)
 
 }
