893000a6fa121fdaac155ef37ea5361ea70191ba
MatteoTemp <matteotempella20@gmail.com>
2024-11-13 12:16:28 +0100

    Aggiunto tok

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
+import (
+	"goEl/lexer"
+)
+
 //cmath "math/cmplx"
 
 func main() {
 
+	cirtcuit_descriprion := "R(CR)"
 
+	cd := lexer.Init(cirtcuit_descriprion)
+
+	if err1 := cd.Tokenize(); err1 != nil {
+		panic("Error reading tokens, ")
+	}
+
+	if err2 := cd.MatchBrakets(); err2 != nil {
+		panic("matching error")
+	}
+
+	cd.Display()
 
 }
