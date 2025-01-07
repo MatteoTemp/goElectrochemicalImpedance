915708d0696ec86ca5f62e391276f45417c7a77d
MatteoTemp <matteotempella20@gmail.com>
2025-01-07 11:51:28 +0100

    Update main.go

--- main.go
+++ main.go
@@ -12,21 +12,11 @@ func main() {
 		Reaction_resistance: dipoles.Resistor{Resistance: 500},
 		Diffusion_impedance: dipoles.Warburg{W_sigma: 500},
 		Double_layer_capacity: dipoles.Constant_phase_Element{
-			Phi:        0.6,
+			Phi:        0.99,
 			TParameter: 1e-6,
 		},
 	}
 	//circuits.Nyquist_plot(circuit1, "OutputFiles/Nyq.csv", 0, 6, 100)
 	circuits.Bode(circuit1, "OutputFiles/Bode.csv", -3, 6, 100)
 	circuits.Nyquist_plot(circuit1, "OutputFiles/Nyq.csv", -0.3, 6, 1000, false)
-
-	circuit2 := circuits.Randles{
-		Solution_rasistance:   dipoles.Resistor{Resistance: 10},
-		Reaction_resistance:   dipoles.Resistor{Resistance: 500},
-		Diffusion_impedance:   dipoles.Warburg{W_sigma: 500},
-		Double_layer_capacity: dipoles.Capacitor{Capacitance: 1e-6},
-	}
-	//circuits.Nyquist_plot(circuit1, "OutputFiles/Nyq.csv", 0, 6, 100)
-	circuits.Bode(circuit2, "OutputFiles/Bode.csv", -3, 6, 100)
-	circuits.Nyquist_plot(circuit2, "OutputFiles/Nyq.csv", -0.3, 6, 1000, false)
 }
